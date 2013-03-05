ba4dc1c938938a0f5a85e4c08a2139f7bf2f9b25
Michael Brooks <mjbrooks@uw.edu>
2013-03-04 19:34:28 -0800

    basic db io

--- app.py
+++ app.py
@@ -1,13 +1,36 @@
 import web
+import db, model
+import appconfig as conf
 
 urls = (
     '/', 'index'
 )
 
-class index:
-    def GET(self):
-        return "Hello, world!"
+web.config.debug = conf.DEBUG
 
+render = web.template.render('templates/')
+
+app = web.application(urls, globals())
+app.add_processor(db.load_sqla)
+
+class index:
+    def get_emails(self):
+        emails = [e.user_email for e in web.ctx.orm.query(model.Poll.user_email)]
+        return emails
+        
+    def GET(self):
+        emails = self.get_emails()
+        return render.index(emails)
+
+    def POST(self):
+        i = web.input()
+        
+        poll = model.Poll()
+        poll.user_email = i.email
+        web.ctx.orm.add(poll)
+        
+        emails = self.get_emails()
+        return render.index(emails)
+        
 if __name__ == "__main__":
-    app = web.application(urls, globals())
     app.run()
