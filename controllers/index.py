671a5636b491e67c96ce4f6a6a0f471edf33fbd3
Michael Brooks <mjbrooks@uw.edu>
2013-03-14 10:35:52 -0700

    update index controller and template

--- controllers/index.py
+++ controllers/index.py
@@ -1,23 +1,11 @@
 import web
+
 from model import Poll
 
 from . import pagerender as render
 
 class index:
-    def get_emails(self):
-        emails = [e.user_email for e in web.ctx.orm.query(Poll.user_email)]
-        return emails
         
     def GET(self):
-        emails = self.get_emails()
-        return render.index(emails)
-
-    def POST(self):
-        i = web.input()
-        
-        poll = Poll()
-        poll.user_email = i.email
-        web.ctx.orm.add(poll)
-        
-        emails = self.get_emails()
-        return render.index(emails)
+        polls = web.ctx.orm.query(Poll).all()
+        return render.index(polls)
