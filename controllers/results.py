431e1fcc4555ca91a42af5f06faf89a292bf17ec
Michael Brooks <mjbrooks@uw.edu>
2013-03-14 10:50:03 -0700

    add display of results

--- controllers/results.py
+++ controllers/results.py
@@ -1,10 +1,18 @@
 import web
 
+from model import Poll
+
 from . import pagerender as render
 
 class results:
+    def _get_poll(self, poll_url_code):
+        poll = web.ctx.orm.query(Poll).filter(Poll.poll_url_code == poll_url_code).first();
+        if poll is None:
+            raise web.ctx.notfound()
+        return poll
+        
     def GET(self, poll_url):
         # look up the poll based on the url
-        poll = None
+        poll = self._get_poll(poll_url)
         
         return render.results(poll)
