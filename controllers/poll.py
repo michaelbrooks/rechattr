6a514f1980a7f7e56822052c41b00b3b5af0e6af
Michael Brooks <mjbrooks@uw.edu>
2013-03-14 10:08:13 -0700

    update poll controller

--- controllers/poll.py
+++ controllers/poll.py
@@ -1,20 +1,36 @@
 import web
+import simplejson as json
 
 from . import pagerender as render
+from model import Poll, Response
 
 class poll:
+    def _get_poll(self, poll_url_code):
+        poll = web.ctx.orm.query(Poll).filter(Poll.poll_url_code == poll_url_code).first();
+        if poll is None:
+            raise web.ctx.notfound()
+        return poll
+
     def GET(self, poll_url):
         # look up the poll based on the url
-        poll = None
+        poll = self._get_poll(poll_url)
         
         # display the poll
         return render.poll(poll)
     
     def POST(self, poll_url):
         # look up the poll based on the url
-        poll = None
+        poll = self._get_poll(poll_url)
+        
+        answers = web.input()
         
         # save the response
+        response = Response()
+        response.poll = poll;
+        response.visit = None;
+        response.comment = None;
+        response.answers = json.dumps(answers);
+        web.ctx.orm.add(response)
         
         # go to the results page
-        web.seeother('/results')
+        web.seeother(poll.results_url())
