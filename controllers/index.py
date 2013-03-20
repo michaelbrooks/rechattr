389735bc6720265b297c8e4fc78cb81fcac924c7
Michael Brooks <mjbrooks@uw.edu>
2013-03-19 18:49:37 -0700

    add tweet display to results

--- controllers/index.py
+++ controllers/index.py
@@ -7,5 +7,5 @@ from . import pagerender as render
 class index:
         
     def GET(self):
-        polls = web.ctx.orm.query(Poll).all()
+        polls = web.ctx.orm.query(Poll).order_by(Poll.created).all()
         return render.index(polls)
