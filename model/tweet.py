a9c86c7379412b08632c78c3455f42ef566a36ff
Michael Brooks <mjbrooks@uw.edu>
2013-03-19 18:29:25 -0700

    add from json constructor to tweet

--- model/tweet.py
+++ model/tweet.py
@@ -3,6 +3,7 @@ from sqlalchemy import ForeignKey
 from sqlalchemy import Integer, String, DateTime, BigInteger
 from sqlalchemy.orm import relationship, backref    
 from datetime import datetime
+from dateutil import parser
 
 # Get the shared base class for declarative ORM
 from . import Base
@@ -31,4 +32,15 @@ class Tweet(Base):
     text = Column(String)
     retweet_of_status_id = Column(BigInteger)
     reply_to_tweet_id = Column(BigInteger)
+    
+    def __init__(self, status_obj):
+        self.id = status_obj['id']
+        self.created = parser.parse(status_obj['created_at'])
+        self.user_id = status_obj['user']['id']
+        self.screen_name = status_obj['user']['screen_name']
+        self.user_name = status_obj['user']['name']
+        self.text = status_obj['text']
+        self.reply_to_tweet_id = status_obj['in_reply_to_status_id']
+        if 'retweet_of_status_obj_id' in status_obj:
+            self.retweet_of_status_obj_id = status_obj['retweet_of_status_id']
     
