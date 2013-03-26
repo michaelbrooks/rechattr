46931691c2e7d578ca2c386c21a940e3a2b6abda
Michael Brooks <mjbrooks@uw.edu>
2013-03-26 01:10:30 -0300

    use web.ctx.auth to get current user

--- controllers/index.py
+++ controllers/index.py
@@ -1,7 +1,6 @@
 import web
 
 from model import Poll
-from libs import Auth
 
 from . import pagerender as render
 
@@ -9,6 +8,5 @@ class index:
         
     def GET(self):
         polls = web.ctx.orm.query(Poll).order_by(Poll.created).all()
-        auth = Auth()
-        user = auth.current_user()
+        user = web.ctx.auth.current_user()
         return render.index(polls, user)
