c9eb7ca5528d7ae2018df37bc6cfb5eaff81c35e
Michael Brooks <mjbrooks@uw.edu>
2013-03-29 02:42:19 -0300

    swipe working nicely

--- static/js/mobile-poll.js
+++ static/js/mobile-poll.js
@@ -16,6 +16,14 @@
         }
     }
     
+    function toggleMovingClass(selection, visible) {
+        if (visible) {
+            selection.addClass('moving');
+        } else {
+            selection.removeClass('moving');
+        }
+    }
+    
     var MobilePoll = function() {
         this.ui = {};
         
@@ -78,13 +86,92 @@
     MobilePoll.prototype.attachInteractions = function() {
         var self = this;
         
-        this.ui.chatter.on('click', function() {
-            self.showChatter();
-        });
+        var width = this.width();
+        var offset = 0;
+        var margin = this.titleHeight();
+        var leftBoundary = 0;
+        var rightBoundary = width;
+        var squeezeRoom = 3;
         
-        this.ui.feedback.on('click', function() {
+        var showChatter = function() {
+            self.showChatter();
+        }
+        var showFeedback = function() {
             self.showFeedback();
-        });
+        }
+        var moveStarted = function(e) {
+            // If the movestart heads off in a upwards or downwards
+            // direction, prevent it so that the browser scrolls normally.
+            if ((e.distX > e.distY && e.distX < -e.distY) ||
+                (e.distX < e.distY && e.distX > -e.distY)) {
+                e.preventDefault();
+                return;
+            }
+
+            // If feedback panel is active is the target, don't continue
+            if (self.showing == 'feedback' &&
+                (self.ui.feedback[0] == e.target || self.ui.feedback.find(e.target).size() > 0)) {
+                e.preventDefault();
+                return;
+            }
+            
+            // To allow the slide to keep step with the finger,
+            // temporarily disable transitions.
+            self.ui.chatter.addClass('no-transition');
+            
+            //Save this value for later
+            width = self.width();
+            margin = self.titleHeight();
+            leftBoundary = 100 * 2 * margin / width - squeezeRoom;
+            rightBoundary = 100 + squeezeRoom;
+            if (self.showing == 'chatter') {
+                offset = 2 * margin - e.startX;
+            } else {
+                offset = width - e.startX;
+            }
+        }
+        
+        var updatePosition = function(e) {
+            // percent moved horizontally
+            var position = e.pageX + offset;
+            var left = 100 * position / width;
+            // var margin = 64;
+            // var percentMargin = 100 * margin / width;
+            // var range = width - margin;
+            
+            left = Math.max(leftBoundary, Math.min(rightBoundary, left));
+            
+            // Move chatter with the finger
+            self.ui.chatter.css('left', left + '%');
+            
+            var physicalLeft = position - margin;
+            
+            toggleMovingClass(self.ui.chatter, physicalLeft < width - 2 * margin);
+            toggleMovingClass(self.ui.feedback, physicalLeft < 2 * margin);
+        }
+        
+        var moveEnded = function(e) {
+            self.ui.chatter.removeClass('no-transition');
+            
+            self.ui.chatter.css('left', '');
+            
+            toggleMovingClass(self.ui.chatter, false);
+            toggleMovingClass(self.ui.feedback, false);
+        }
+        
+        this.ui.chatter.on('click', showChatter)
+        .on('swipeleft', showChatter)
+        .on('swiperight', showFeedback)
+        .on('movestart', moveStarted)
+        .on('move', updatePosition)
+        .on('moveend', moveEnded);
+        
+        this.ui.feedback.on('click', showFeedback)
+        .on('swiperight', showFeedback)
+        .on('movestart', moveStarted)
+        .on('move', updatePosition)
+        .on('moveend', moveEnded);
+        
     }
     
     rechattr.classes.MobilePoll = MobilePoll;
