f9ac32fd6df6484205318a453af7a6c3c9814404
Michael Brooks <mjbrooks@uw.edu>
2013-04-05 00:52:10 -0700

    fancy schmancy timeline

--- static/js/edit.js
+++ static/js/edit.js
@@ -5,16 +5,60 @@
     var DATE_PICKER_FORMAT = 'm/dd/yyyy'
     
     var DEFAULT_DURATION = 60*60; //1 hour in seconds
-    
-    var INTERVAL_FIELD_SELECTOR = '.interval-field';
-    
+    var TIMELINE_WRAPPER_SELECTOR = '.timeline-wrapper';
+    var QUESTION_LIST_SELECTOR = '.question-list';
     
     var EditApp = function() {
         var self = this;
         
-        this.intervalSelector = new rechattr.util.IntervalSelection($(INTERVAL_FIELD_SELECTOR));
+        this.initUI();
+        
+        this.timeline = new rechattr.util.Timeline(this.ui.timelineWrapper);
+        
+        this.attachTimelineEvents();
+    }
+    
+    EditApp.prototype.initUI = function() {
+        this.ui = {};
+        
+        this.ui.timelineWrapper = $(TIMELINE_WRAPPER_SELECTOR);
+        
+        this.ui.questionList = $(QUESTION_LIST_SELECTOR);
+    }
+    
+    EditApp.prototype.attachTimelineEvents = function() {
+        var self = this;
+        $(this.timeline).on('new-bead', function(e, bead) {
+            
+            var question = new Question();
+            self.ui.questionList.append(question.render());
+            
+            bead.data('question', question);
+        })
+        .on('select', function(e, bead) {
+            var question = bead.data('question');
+            question.message = 'yay';
+            question.render();
+        })
+        .on('deselect', function(e, bead) {
+            var question = bead.data('question');
+            question.message = 'aw...';
+            question.render();
+        });
     }
 
+    var Question = function() {
+        this.message = 'asdf';
+        
+        this.$el = $('<div>').addClass('question');
+    }
+    
+    Question.prototype.render = function() {
+        this.$el.text(this.message);
+        
+        return this.$el;
+    }
+    
     rechattr.classes.EditApp = EditApp;
     return EditApp;
 })()
---
+++ static/js/edit/timeline.js
@@ -0,0 +1,138 @@
+(function() {
+
+    var TIME_FORMAT = 'h:mma';
+    var DATE_FORMAT = 'M/DD/YYYY';
+    var DATE_PICKER_FORMAT = 'm/dd/yyyy'
+    
+    var DEFAULT_DURATION = 60*60; //1 hour in seconds
+    
+    var TIMELINE_SELECTOR = '.timeline';
+    var TIMELINE_LINE_SELECTOR = '.timeline-line';
+    var TIMELINE_FOCUS_SELECTOR = '.timeline-focus';
+    var TIMELINE_BEAD_SELECTOR = '.timeline-bead';
+    
+    var Timeline = function(wrapper) {
+        this.initUI(wrapper);
+        
+        this.attachEvents();
+    }
+    
+    Timeline.prototype.initUI = function(wrapper) {
+        this.$ = $(this);
+        this.ui = {};
+        
+        this.ui.wrapper = wrapper;
+        this.ui.timeline = this.ui.wrapper.find(TIMELINE_SELECTOR);
+        this.ui.timelineLine = this.ui.timeline.find(TIMELINE_LINE_SELECTOR);
+        this.ui.timelineFocus = this.ui.timeline.find(TIMELINE_FOCUS_SELECTOR);
+        this.ui.timelineBeads = this.ui.timeline.find(TIMELINE_BEAD_SELECTOR);
+    }
+    
+    Timeline.prototype.createBead = function(x, y) {
+        var percentThrough = x / this.ui.wrapper.width();
+        
+        var bead = $('<div>')
+        .addClass('timeline-bead')
+        .css('left', x);
+        
+        this.trigger('new-bead', bead);
+        
+        this.ui.timeline.append(bead);
+        this.ui.timelineBeads = this.ui.timelineBeads.add(bead);
+        
+        this.selectBead(bead);
+    }
+    
+    Timeline.prototype.trigger = function(eventName, data) {
+        this.$.trigger(eventName, [data]);
+    }
+    
+    Timeline.prototype.selectBead = function(bead) {
+        if (this.selected) {
+            this.trigger('deselect', this.selected);
+            this.selected.removeClass('selected');
+        }
+    
+        this.trigger('select', bead);
+        
+        this.selected = bead;
+        this.selected.addClass('selected');
+    }
+
+    Timeline.prototype.startDragging = function(bead) {
+        this.trigger('drag-start', bead);
+        
+        this.dragging = bead;
+        this.dragging.addClass('dragging');
+        this.ui.timelineFocus.removeClass('in');
+        
+        this.selectBead(bead);
+    }
+    
+    Timeline.prototype.stopDragging = function(bead) {
+        this.trigger('drag-stop', bead);
+        
+        this.dragging.removeClass('dragging');
+        this.dragging = null;
+    }
+    
+    Timeline.prototype.attachEvents = function() {
+        var self = this;
+        this.ui.wrapper.on('mousemove', function(e) {
+            var width = self.ui.wrapper.width();
+            var offset = self.ui.wrapper.offset();
+            var x = e.pageX - offset.left;
+            var y = e.pageY - offset.top;
+            
+            if (x < 0) {
+                x = 0;
+            } else if (x > width) {
+                x = width
+            }
+            
+            var positioned = self.ui.timelineFocus;
+            
+            if (self.dragging) {
+                positioned = self.dragging;
+            }
+            
+            positioned.css('left', x);
+        })
+        .on('mouseover', function(e) {
+            if (!self.dragging) {
+                self.ui.timelineFocus.addClass('in');
+            }
+        })
+        .on('mouseout', function(e) {
+            self.ui.timelineFocus.removeClass('in');
+        })
+        .on('click', function(e) {
+            var offset = self.ui.wrapper.offset();
+            var x = e.pageX - offset.left;
+            var y = e.pageY - offset.top;
+            
+            self.createBead(x, y);
+        })
+        .on('click', TIMELINE_BEAD_SELECTOR, function(e) {
+            self.selectBead($(this));
+            e.preventDefault();
+            return false;
+        })
+        .on('mousedown', TIMELINE_BEAD_SELECTOR, function(e) {
+            self.startDragging($(this))
+            
+            e.preventDefault();
+            return false;
+        });
+        
+        $(document).on('mouseup', function(e) {
+            if (self.dragging) {
+                self.stopDragging();
+            }
+        });
+        
+    }
+    
+    rechattr.util.Timeline = Timeline;
+    return Timeline;
+})()
