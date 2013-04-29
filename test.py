c752f7d3e138fc9bc101acc79dab7f7f5d9d6b5b
Michael Brooks <mjbrooks@uw.edu>
2013-04-28 21:41:27 -0700

    update test.py

--- test.py
+++ test.py
@@ -1,18 +1,21 @@
+from __future__ import print_function
 import doctest
 
-
 def test_all(group, moduleNames):
-    print "Beginning tests for '%s'" % (group)
+    output = ["Beginning tests for '%s'" % (group)]
 
     fail, tests = 0, 0
     for m in moduleNames:
         module = (__import__(m, fromlist=['this is a hack!']))
         f, t = doctest.testmod(module, report=True)
-        print "  %s out of %s tests passed in '%s'" % (t - f, t, module.__name__)
+        output.append("  %s out of %s tests passed in '%s'" % (t - f, t, module.__name__))
         fail += f
         tests += t
 
-    print "Summary for '%s': Passed %s out of %s tests\n" % (group, tests - fail, tests)
+    output.append("Summary for '%s': Passed %s out of %s tests: %d failures" % (group, tests - fail, tests, fail))
+
+    print()
+    map(print, output)
 
 # Run model tests
 test_all('model', [
